12229e162cae44628359370b57a428ac5ac04409
Raphael Margueron <raphael.margueron@gmail.com>
2025-02-19 18:43:54 +0100

    add challenges fetcher + add challenges in team builder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,26 @@
 <script lang="ts">
 	import { Button, InputText } from '$lib/components';
+	import Tooltip from '$lib/components/Tooltip.svelte';
 	import champions_json from 'data/cache/champion.cache.json';
+	import challenges_json from 'data/challenges.json';
+
+	const challenges = Object.entries(challenges_json).map((c) => c[1]);
+
+	const groups = [
+		{
+			main: challenges_json[303400],
+			challenges: challenges.filter((c) => challenges_json[303400].childrenIds.includes(c.id))
+		},
+		{
+			main: challenges_json[303500],
+			challenges: challenges.filter((c) => challenges_json[303500].childrenIds.includes(c.id))
+		}
+	];
+
+	function getSize(challenge: any) {
+		if ([303407, 303408].includes(challenge.id) || challenge.parentId == 303500) return 5;
+		else return 3;
+	}
 
 	const championsAll = Object.entries(champions_json.data).map((c) => c[1]);
 	const championsMap = new Map(Object.entries(champions_json.data));
@@ -49,7 +69,6 @@
 		oninput={filter}
 		onkeypress={filterKey}
 	/>
-
 	<Button
 		class="m-3"
 		title="Find compositions that satify the current selection (selected champions and challenges)."
@@ -61,9 +80,9 @@
 			{@const championSelected = championsSelected.at(i) ?? ''}
 			{@const champion = championsMap.get(championSelected)}
 
-			<div class="m-1.5 h-11 w-11">
+			<div class={['m-1.5', 'h-11', 'w-11', champion == undefined ? 'p-[11px]' : '']}>
 				{#if champion == undefined}
-					<div class="v-full h-full rounded-full bg-white/50 p-3"></div>
+					<div class="v-full h-full rounded-full bg-white/50"></div>
 				{:else}
 					<button class="cursor-pointer" onclick={(e) => championClick(e, champion.id)}>
 						<img src={`/img/cache/${champion?.image.full}`} alt={champion.name} />
@@ -72,15 +91,6 @@
 			</div>
 		{/each}
 	</div>
-	<!-- <div class="col col-md-auto mt-2">
-		<span id="attribution_score">0.00/5.00</span>
-		<span class="tooltip_ hoverable"
-			>?<span class="tooltip_text tooltip_right"
-				>Attribution score, how well the comp is meta. The closer to 5 the closer each champion play
-				its natural position</span
-			></span
-		>
-	</div> -->
 	<Button class="m-3" title="Clear selections" onclick={clear}>
 		<i class="fa-solid fa-fw fa-trash"></i> Clear
 	</Button>
@@ -90,22 +100,95 @@
 </div>
 
 <div class="flex">
-	<div class="flex flex-col">
-		{#each Array.from(Array(10).keys()) as i}
-			<input type="checkbox" />
-		{/each}
-	</div>
+	<table class="shrink grow">
+		<thead>
+			<tr>
+				<th class="px-2"></th>
+				<th class="px-2 text-right">#</th>
+				<th class="px-2 text-left">Challenges</th>
+				<th class="px-2"></th>
+				<th class="px-2 text-left">Selection</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each groups as group}
+				{@const main = group.main}
+				<tr>
+					<td class="px-2"></td>
+					<td class="px-2"></td>
+					<th class="px-2 text-left">{main.name}</th>
+					<td class="px-2 text-left">
+						<Tooltip text="?">
+							{main.description}
+						</Tooltip>
+					</td>
+				</tr>
+				{#each group.challenges as challenge}
+					{@const championsId = challenge.availableIds.map((i) => i.toString())}
+					{@const championsSelectedChallenge = championsSelected
+						.filter((championKey) =>
+							championsId.includes(championsMap?.get(championKey)?.key ?? '')
+						)
+						.map((key) => championsMap.get(key))}
+					{@const missingDots = getSize(challenge) - championsSelectedChallenge.length}
+					<tr>
+						<td class="px-2 pt-0.5 text-right"
+							><input
+								type="checkbox"
+								id={`challenge_cb_${challenge.id}`}
+								class="cursor-pointer"
+							/></td
+						>
+						<td class="px-2 text-right">
+							<label for={`challenge_cb_${challenge.id}`} class="cursor-pointer">
+								{challenge.availableIds.length}
+							</label>
+						</td>
+						<td class="px-2 text-left">
+							<label for={`challenge_cb_${challenge.id}`} class="cursor-pointer text-nowrap">
+								{challenge.name}
+							</label>
+						</td>
+						<td class="px-2 text-left">
+							<Tooltip text="?">
+								{challenge.description}
+							</Tooltip>
+						</td>
+						<td class="px-2 text-left">
+							<div class="flex items-center">
+								{#each championsSelectedChallenge as championSelectedChallenge}
+									<div class="mx-0.5 h-[20px] w-[20px]">
+										<img
+											src={`/img/cache/${championSelectedChallenge?.image.full}`}
+											alt={championSelectedChallenge?.name}
+										/>
+									</div>
+								{/each}
+								{#each Array.from(Array(missingDots).keys()) as i}
+									<div class="mx-0.5 h-[20px] w-[20px] p-[5px]">
+										<div class="v-full h-full rounded-full bg-white/50"></div>
+									</div>
+								{/each}
+							</div>
+						</td>
+					</tr>
+				{/each}
+			{/each}
+		</tbody>
+	</table>
 
-	<div class="flex flex-wrap justify-center">
-		{#each champions as champion}
-			<button
-				class="m-1 ring-amber-400/75 transition-all duration-75"
-				class:ring-2={championsSelected.includes(champion.id)}
-				class:cursor-pointer={canAdd || championsSelected.includes(champion.id)}
-				onclick={(e) => championClick(e, champion.id)}
-			>
-				<img src={`/img/cache/${champion.image.full}`} alt={champion.name} class="w-18" />
-			</button>
-		{/each}
+	<div>
+		<div class="flex flex-wrap justify-center">
+			{#each champions as champion}
+				<button
+					class="m-1 ring-amber-400/75 transition-all duration-75"
+					class:ring-2={championsSelected.includes(champion.id)}
+					class:cursor-pointer={canAdd || championsSelected.includes(champion.id)}
+					onclick={(e) => championClick(e, champion.id)}
+				>
+					<img src={`/img/cache/${champion.image.full}`} alt={champion.name} class="w-16" />
+				</button>
+			{/each}
+		</div>
 	</div>
 </div>
---
+++ update_challenges.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from requests import get, auth
+import json
+
+game_path = "C:/Riot Games/League of Legends/"
+certif_path = "./riotgames.pem"
+
+def get_endpoint(game_folder):
+    values = open(game_folder / Path("lockfile")).read().split(":")
+    return (f"{values[4]}://127.0.0.1:{values[2]}", ("riot", values[3]))
+
+
+def query(route):
+    try:
+        url, creds = get_endpoint(Path(game_path))
+        basic = auth.HTTPBasicAuth(*creds)
+        answer = get(url + route, auth=basic, verify=Path(certif_path))
+        return json.loads(answer.content)
+    except FileNotFoundError:
+        print("Client not open")
+
+challenges = query(f"/lol-challenges/v1/challenges/local-player")
+
+to_keep = [
+    "availableIds", "capstoneGroupId", "capstoneGroupName", "category", "childrenIds", "description", "descriptionShort", "gameModes", "hasLeaderboard", "id", "idListType", "isCapstone", "isReverseDirection", "name", "parentId", "parentName", "retireTimestamp", "source", "thresholds"
+]
+
+for challenge_k, challenge in challenges.items():
+    for k, v in list(challenge.items()):
+        if k not in to_keep:
+            del challenge[k]
+
+json.dump(challenges, open("src/data/challenges.json", "w"), indent=4)
