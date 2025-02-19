79be42df729ab65a696a43732caf2d10ecf86e59
Raphael Margueron <raphael.margueron@gmail.com>
2025-02-19 19:58:57 +0100

    add fabfile

---
+++ fabfile.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import click
+import fabric
+
+
+def deploy(c):
+    with c.cd("/opt/beta.tahm-ken.ch"):
+        c.run("git fetch")
+        c.run("git status")
+        if not click.confirm("Stash and deploy to main ?", default=True):
+            exit()
+        c.run("git stash")
+        c.run("git checkout main")
+        c.run("git pull")
+        c.run("docker compose up -d --build")
+
+
+c = fabric.Connection(host="beta.tahm-ken.ch", user="root", port=22)
+deploy(c)
