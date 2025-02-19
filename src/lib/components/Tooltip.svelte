1381b8ec3cd37decb6ba1b1a9d7d992dfafc6953
Raphael Margueron <raphael.margueron@gmail.com>
2025-02-19 18:43:54 +0100

    add challenges fetcher + add challenges in team builder

---
+++ src/lib/components/Tooltip.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	let { children, class: propClasses = '', text, ...props } = $props();
+	let visible = $state(false);
+</script>
+
+<span
+	{...props}
+	class={['cursor-help', "border-b-1 border-dashed border-white", propClasses]}
+	onmouseover={() => (visible = true)}
+	onmouseleave={() => (visible = false)}
+>
+	{text}
+	{#if visible}
+		<div class="absolute bg-black/95 p-3 -mt-10 ml-5 z-10 pointer-events-none">
+			{@render children()}
+		</div>
+	{/if}
+</span>
