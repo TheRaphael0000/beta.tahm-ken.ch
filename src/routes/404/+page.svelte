bb3ca14cfa71b3060dec64ee5a6fabfbe2993d32
Raphael Margueron <raphael.margueron@gmail.com>
2025-02-26 14:30:47 +0100

    add 404 page

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Article from "$lib/components/Article.svelte";
+	import H1 from "$lib/components/H1.svelte";
+
+</script>
+
+<svelte:head>
+    <title>404</title>
+</svelte:head>
+
+<Article>
+    <H1>404 - Not found</H1>
+</Article>
