bb2fec4e3db0d806eed1e1194b55437bde96c709
Raphael Margueron <raphael.margueron@gmail.com>
2025-07-17 16:42:09 +0200

    update challenge with yunara + composition work in progress page

--- src/routes/compositions/+page.svelte
+++ src/routes/compositions/+page.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
+	import { Article, H1 } from "$lib/components";
+
 </script>
 
 <svelte:head>
     <title>Compositions</title>
 </svelte:head>
+
+
+<Article>
+	<H1>Compositions</H1>
+
+	<p>
+		This page is still work in progress...
+	</p>
+</Article>
