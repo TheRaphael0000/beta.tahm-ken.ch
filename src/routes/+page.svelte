e2b62bf2ec40fccc1a81f44d30a76df87990e40b
Raphael Margueron <raphael.margueron@gmail.com>
2025-07-20 12:30:54 +0200

    update home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,59 +6,66 @@
 	<H1>Welcome to Tahm-Ken.ch</H1>
 
 	<p>
-		The goal of this website is to help you to complete <b>Harmony</b> and <b>Globetrotter</b> challenges
-		on League of Legends!
+		This website is designed to help you complete <b>Harmony</b> and <b>Globetrotter</b> challenges in
+		League of Legends!
 	</p>
 
-	<H2>Site map</H2>
-	<p>Three tools are available:</p>
+	<H2>Our Tools</H2>
+	<p>We offer three main tools to assist you:</p>
 	<ul class="ml-8 list-disc">
 		<li>
-			<Link href="/multisearch">Multisearch</Link><br />Help you find which challenges are missing
-			in your premade.
+			<Link href="/multisearch">Multisearch</Link>
+			<br />
+			Find out which challenges are missing from your premade group.
 		</li>
 		<li>
-			<Link href="/team_builder">Team Builder</Link><br />Help you find which champion you should
-			pick in order to complete the challenges
+			<Link href="/team_builder">Team Builder</Link>
+			<br />
+			Discover which champions you should pick to complete specific challenges.
 		</li>
 		<li>
-			<Link href="/compositions">Compositions</Link><br />Pre-computed compositions to maximize
-			challenges completions.
+			<Link href="/compositions">Compositions</Link>
+			<br />
+			Access pre-computed team compositions optimized for maximizing challenge completion.
 		</li>
 	</ul>
 
 	<H2>Community</H2>
 	<p>
-		If you are looking for teammates for these challenge or other challenges, check the <Link
-			href="/community">Community</Link
+		Looking for teammates for these or other challenges? Check out our <Link href="/community"
+			>Community</Link
 		> page.
 	</p>
 
 	<p>
-		If you find <b>bugs</b> or you have <b>improvements</b> for the website, please tell us on the
-		discord server. [<Link href="https://discord.gg/aHs3uDraNU">Discord</Link>]
+		If you encounter any <b>bugs</b> or have <b>suggestions</b> for website <b>improvements</b>,
+		please let us know on our discord server. [<Link href="https://discord.gg/aHs3uDraNU"
+			>Discord</Link
+		>]
 	</p>
 
-	<H2>Donation</H2>
+	<H2>Support Us</H2>
 	<p>
-		The website <b>doesn't run ads</b> and we are not collecting any data except page visit
-		statistics. If you still want to support us you can donate. [<Link
+		Tahm-Ken.ch is <b>ad-free</b> and we don't collect any personal data, only page visit
+		statistics (using <Link href="https://github.com/umami-software/umami">Umami</Link>). If you'd like to support our work, you can <b>donate</b> here. [<Link
 			href="https://ko-fi.com/theraphael0000">Donation page</Link
-		>]. By donating won't get any advantage expect have your name in the multisearch search bar
-		placeholder (if you want to).
+		>]
 	</p>
 	<p>
-		If you are concerned about <b>Tracking</b> and
-		<b>Advertisement</b>. I recommend you to read this well written article: <Link
-			href="https://lichess.org/ads">https://lichess.org/ads</Link
-		>
+		Donating offers no special advantages, though we can display your name in the Multisearch bar
+		placeholder if you wish.
+	</p>
+	<p>
+		For those concerned about tracking and advertisements, we recommend reading this insightful
+		article:
+		<Link href="https://lichess.org/ads">https://lichess.org/ads</Link>
 	</p>
 
 	<H2>Source Code</H2>
 	<p>
-		Tahm-Ken.ch is completely <b>open source</b>, which mean if you want to understand how it works or improve
-		it, feel free to do so! [<Link href="https://github.com/TheRaphael0000/beta.tahm-ken.ch/"
-			>Source Code</Link
+		Tahm-Ken.ch is entirely <b>open source</b>. Feel free to explore the code to understand how it
+		works or contribute to its improvement! [<Link
+			href="https://github.com/TheRaphael0000/beta.tahm-ken.ch/">Source Code</Link
 		>]
 	</p>
 </Article>
